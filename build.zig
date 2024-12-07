48b8ad4a6587416a965483aaa6158ece6c68eb4c
Marc Zweiler <marc.zweiler@outlook.at>
2024-12-07 19:07:04 +0100

    Added compile flags to the build file

--- build.zig
+++ build.zig
@@ -5,25 +5,38 @@ pub fn build(b: *std.Build) void {
     // Options
     //const tests = b.option(bool, "Test", "Runs all builtin tests (default: false)") orelse false;
 
-    const source_files = [_][]const u8{ "src/error/error.cpp", "src/lexer/lexer.cpp", "src/linker/linker.cpp", "src/parser/parser.cpp", "src/parser/signature.cpp", "src/debug.cpp" };
+    const source_files = [_][]const u8{
+        "src/error/error.cpp", // error
+        "src/lexer/lexer.cpp", // lexer
+        "src/linker/linker.cpp", //linker
+        "src/parser/parser.cpp", //parser
+        "src/parser/signature.cpp", // signature
+        "src/debug.cpp", // debug
+    };
+    const compile_flags = [_][]const u8{
+        "-g", // generate debug information like stack traces
+        "-O0", // disable optimization for easier debugging
+        "-fno-omit-frame-pointer", // ensures that the frame pointer is reserved for accurate stack traces
+        "-DEBUG", // enables assertions and other debugging-related macros
+    };
 
     // --- LINUX ---
-    add_linux_executable(b, optimization, &source_files);
+    add_linux_executable(b, optimization, &source_files, &compile_flags);
     // --- WINDOWS ---
-    add_windows_executable(b, optimization, &source_files);
+    add_windows_executable(b, optimization, &source_files, &compile_flags);
     // --- MACOS ---
-    add_macos_executable(b, optimization, &source_files);
+    add_macos_executable(b, optimization, &source_files, &compile_flags);
     // --- UNIT TESTS ---
-    add_unit_tests(b, optimization, &source_files);
+    add_unit_tests(b, optimization, &source_files, &compile_flags);
 }
 
-pub fn add_linux_executable(b: *std.Build, optimization: std.builtin.OptimizeMode, source_files: []const []const u8) void {
+pub fn add_linux_executable(b: *std.Build, optimization: std.builtin.OptimizeMode, source_files: []const []const u8, flags: []const []const u8) void {
     const target_linux = b.resolveTargetQuery(.{ .cpu_arch = .x86_64, .os_tag = .linux });
     const flintc_linux = b.addExecutable(.{ .name = "flintc-linux", .target = target_linux, .optimize = optimization });
     flintc_linux.linkLibCpp();
     flintc_linux.addIncludePath(b.path("src"));
-    flintc_linux.addCSourceFile(.{ .file = .{ .cwd_relative = "src/main.cpp" } });
-    flintc_linux.addCSourceFiles(.{ .files = source_files });
+    flintc_linux.addCSourceFile(.{ .file = .{ .cwd_relative = "src/main.cpp" }, .flags = flags });
+    flintc_linux.addCSourceFiles(.{ .files = source_files, .flags = flags });
     b.installArtifact(flintc_linux);
 
     // Run the flint-linux file
@@ -36,13 +49,13 @@ pub fn add_linux_executable(b: *std.Build, optimization: std.builtin.OptimizeMod
     run_step_linux.dependOn(&run_linux.step);
 }
 
-pub fn add_macos_executable(b: *std.Build, optimization: std.builtin.OptimizeMode, source_files: []const []const u8) void {
+pub fn add_macos_executable(b: *std.Build, optimization: std.builtin.OptimizeMode, source_files: []const []const u8, flags: []const []const u8) void {
     const target_mac = b.resolveTargetQuery(.{ .cpu_arch = .aarch64, .os_tag = .macos });
     const flintc_mac = b.addExecutable(.{ .name = "flintc-mac", .target = target_mac, .optimize = optimization });
     flintc_mac.linkLibCpp();
     flintc_mac.addIncludePath(b.path("src"));
-    flintc_mac.addCSourceFile(.{ .file = .{ .cwd_relative = "src/main.cpp" } });
-    flintc_mac.addCSourceFiles(.{ .files = source_files });
+    flintc_mac.addCSourceFile(.{ .file = .{ .cwd_relative = "src/main.cpp" }, .flags = flags });
+    flintc_mac.addCSourceFiles(.{ .files = source_files, .flags = flags });
     b.installArtifact(flintc_mac);
 
     // Run the flint-linux file
@@ -55,7 +68,7 @@ pub fn add_macos_executable(b: *std.Build, optimization: std.builtin.OptimizeMod
     run_step_mac.dependOn(&run_mac.step);
 }
 
-pub fn add_windows_executable(b: *std.Build, optimization: std.builtin.OptimizeMode, source_files: []const []const u8) void {
+pub fn add_windows_executable(b: *std.Build, optimization: std.builtin.OptimizeMode, source_files: []const []const u8, flags: []const []const u8) void {
     const target_windows = b.resolveTargetQuery(.{ .cpu_arch = .x86_64, .os_tag = .windows });
     const flintc_windows = b.addExecutable(.{
         .name = "flintc-windows",
@@ -65,8 +78,8 @@ pub fn add_windows_executable(b: *std.Build, optimization: std.builtin.OptimizeM
     });
     flintc_windows.linkLibCpp();
     flintc_windows.addIncludePath(b.path("src"));
-    flintc_windows.addCSourceFile(.{ .file = .{ .cwd_relative = "src/main.cpp" } });
-    flintc_windows.addCSourceFiles(.{ .files = source_files });
+    flintc_windows.addCSourceFile(.{ .file = .{ .cwd_relative = "src/main.cpp" }, .flags = flags });
+    flintc_windows.addCSourceFiles(.{ .files = source_files, .flags = flags });
     b.installArtifact(flintc_windows);
 
     // Run the flint-windows file
@@ -79,7 +92,7 @@ pub fn add_windows_executable(b: *std.Build, optimization: std.builtin.OptimizeM
     run_step_windows.dependOn(&run_windows.step);
 }
 
-pub fn add_unit_tests(b: *std.Build, optimization: std.builtin.OptimizeMode, source_files: []const []const u8) void {
+pub fn add_unit_tests(b: *std.Build, optimization: std.builtin.OptimizeMode, source_files: []const []const u8, flags: []const []const u8) void {
     const target_linux = b.resolveTargetQuery(.{ .cpu_arch = .x86_64, .os_tag = .linux });
     const unit_tests = b.addTest(.{
         .name = "test",
@@ -89,8 +102,8 @@ pub fn add_unit_tests(b: *std.Build, optimization: std.builtin.OptimizeMode, sou
     });
     unit_tests.linkLibCpp();
     unit_tests.addIncludePath(b.path("src"));
-    unit_tests.addCSourceFiles(.{ .files = source_files });
-    unit_tests.addCSourceFile(.{ .file = .{ .cwd_relative = "src/tests.cpp" } });
+    unit_tests.addCSourceFiles(.{ .files = source_files, .flags = flags });
+    unit_tests.addCSourceFile(.{ .file = .{ .cwd_relative = "src/tests.cpp" }, .flags = flags });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
     run_unit_tests.has_side_effects = true;
