abcbb4a7440688224bd59875214f2b37201839e9
Marc Zweiler <marc.zweiler@outlook.at>
2024-12-24 19:55:32 +0100

    Completely changed the linker, which job it is to use the resolver to resolve use statements in each file and add all files to the file_map of the Resolver

--- include/linker/linker.hpp
+++ include/linker/linker.hpp
@@ -1,20 +1,13 @@
 #ifndef __LINKER_HPP__
 #define __LINKER_HPP__
 
-#include "../lexer/token_context.hpp"
-#include "../types.hpp"
+#include "parser/ast/file_node.hpp"
 
-#include <optional>
-#include <string>
-
+/// Linker
+///     The linker is responsible for linking all .ll files together and creating the executable from the .ll files
 class Linker {
   public:
-    static void link(token_list &tokens, std::string cwd);
-
-  private:
-    static std::optional<token_list> get_imports_from(const token_list &tokens);
-
-    static void link_next_use(token_list &source_tokens, const TokenContext &use_path);
+    static void resolve_links(const FileNode &file_node);
 };
 
 #endif
--- src/linker.cpp
+++ src/linker.cpp
@@ -1,84 +1,45 @@
 #include "linker/linker.hpp"
 #include "error/error.hpp"
 #include "error/error_type.hpp"
-#include "lexer/lexer.hpp"
-#include "types.hpp"
+#include "parser/parser.hpp"
+#include "resolver/resolver.hpp"
 
-#include <optional>
-#include <string>
-#include <vector>
+#include <variant>
 
-/// link
-///     Goes through all the imports in the file and scans all the imported files, and links them again, and so on.
-///     Replaces all the 'use "..."' tokens inside the tokens list with the content of the imported file recursively
-void Linker::link(token_list &tokens, std::string cwd) {
-    // check if there is any imports present in the current file
-    std::optional<token_list> paths_maybe = get_imports_from(tokens);
-    // only continue when the list of tokens contains any imports
-    if (!paths_maybe.has_value()) {
-        return;
-    }
-    token_list paths = paths_maybe.value();
+/// resolve_links
+///     Takes a main file and resolves all file imports, causing the AST generation of all used files
+void Linker::resolve_links(const FileNode &file_node) {
+    Resolver::add_dependencies_and_file(file_node);
 
-    for (const TokenContext &path : paths) {
-        link_next_use(tokens, path);
-    }
-}
-
-/// check_for_imports
-///     Goes through a given list of tokens and returns all the paths of included files inside this token list, if there
-///     are any inclusions at all. When no inclusion ('use') is present, None is returned.
-std::optional<token_list> Linker::get_imports_from(const token_list &tokens) {
-    token_list paths;
-    bool last_was_use = false;
-    for (const TokenContext &token : tokens) {
-        if (token.type == TOK_USE) {
-            last_was_use = true;
-            continue;
-        }
-        if (last_was_use) {
-            if (token.type == TOK_STR_VALUE) {
-                last_was_use = false;
-                paths.push_back(token);
+    auto iterator = Resolver::get_file_map().begin();
+    while (iterator != Resolver::get_file_map().end()) {
+        std::vector<dependency> deps = Resolver::get_dependency_map().at(iterator->first);
+        bool file_added = false;
+        // Check if the dependency is a library or a file
+        for (const auto &dep : deps) {
+            if (std::holds_alternative<std::vector<std::string>>(dep)) {
+                // Library reference
+                auto lib_dep = std::get<std::vector<std::string>>(dep);
+                // TODO: Implement the fetching (and pre-fetching into local cache) of FlintHub Libraries
+                // TODO: Create FlintHub at all first, lol
+                throw_err(ERR_NOT_IMPLEMENTED_YET);
             } else {
-                throw_err(ERR_EXPECTED_PATH_AFTER_USE);
+                // File path
+                auto file_dep = std::get<std::pair<std::string, std::string>>(dep);
+                if (Resolver::get_file_map().find(file_dep.second) == Resolver::get_file_map().end()) {
+                    // File is not yet part of the dependency tree, so lets parse and add it!
+                    std::string file_path = file_dep.first + file_dep.second;
+                    FileNode file_node = Parser::parse_file(file_path);
+                    Resolver::add_dependencies_and_file(file_node);
+
+                    // Restart iteration from the beginning of the map
+                    iterator = Resolver::get_file_map().begin();
+                    file_added = true;
+                }
             }
         }
-    }
-
-    if (paths.empty()) {
-        return std::nullopt;
-    }
-    return paths;
-}
-
-/// link_next_use
-///     Goes to the line of the import, removes the import (the provided use) from the token list and inserts all the
-///     scanned tokens of the file to insert into the token list
-void Linker::link_next_use(token_list &source_tokens, const TokenContext &use_path) {
-    // first, lets scan the imported file
-    std::string path = use_path.lexme;
-    Lexer lexer = Lexer(path);
-    token_list file_tokens = lexer.scan();
-    // Check if the scanned file has imports, resolve those first
-    Linker::link(file_tokens, path);
-    // remove the 'use "...";' from the source tokens list, then insert the new file tokens into the source tokens list.
-    for (auto token = source_tokens.begin(); token != source_tokens.end();) {
-        if (token->line == use_path.line) {
-            if (token->lexme == use_path.lexme) {
-
-                // erasing use
-                source_tokens.erase(token - 1);
-                // erasing "..."
-                source_tokens.erase(token - 1);
-                // erasing ;
-                source_tokens.erase(token - 1);
-                // inserting all the new tokens
-                source_tokens.insert(token - 1, file_tokens.begin(), file_tokens.end());
-                // exiting the loop
-                break;
-            }
+        if (!file_added) {
+            ++iterator;
         }
-        token += 1;
     }
 }
