10b1833372aea6034de902e2411c61b033a02f2f
Marc Zweiler <marc.zweiler@outlook.at>
2025-01-07 23:08:54 +0100

    Completely overhauled the 'main.cpp' file and moved the CommandLineParser into its own file The main file now is waay more robust and CLI parsing is now actually decoupled from compilation Added ability to output the IR code to a .ll file (the output.ll file gets cleaned up automatically now)

---
+++ include/cli_parser.hpp
@@ -0,0 +1,116 @@
+#ifndef __CLI_PARSER_HPP__
+#define __CLI_PARSER_HPP__
+
+#include "error/error.hpp"
+#include <filesystem>
+#include <iostream>
+#include <sys/types.h>
+#include <vector>
+
+/// CommandLineParser
+///     Parses all the command line arguments and saves their values locally, accessible from outside
+class CommandLineParser {
+  public:
+    CommandLineParser(int argc, char *argv[]) {
+        // Convert the char* argv[] to a vector of strings
+        args.reserve(argc - 1);
+        for (size_t i = 1; i < argc; ++i) {
+            args.emplace_back(argv[i]);
+        }
+    }
+
+    int parse() {
+        // Iterate through all command-line arguments
+        std::filesystem::path cwd_path = std::filesystem::current_path();
+        for (size_t i = 0; i < args.size(); ++i) {
+            std::string arg = args[i];
+
+            if (arg == "--help" || arg == "-h") {
+                print_help();
+                return 1;
+            } else if (arg == "--file" || arg == "-f") {
+                if (!n_args_follow(i + 1, "<file>", arg)) {
+                    return 1;
+                }
+                source_file_path = get_absolute(cwd_path, args.at(i + 1));
+                ++i;
+            } else if (arg == "--out" || arg == "-o") {
+                if (!n_args_follow(i + 1, "<file>", arg)) {
+                    return 1;
+                }
+                out_file_path = get_absolute(cwd_path, args.at(i + 1));
+                i++;
+            } else if (arg == "--flags") {
+                if (!n_args_follow(i + 1, "\"[flags]\"", arg)) {
+                    return 1;
+                }
+                compile_flags = args.at(i + 1);
+                if (compile_flags.at(0) != '"' || compile_flags.at(compile_flags.length() - 1) != '"') {
+                    throw_err(ERR_CLI_PARSING);
+                }
+                // Remove the " symbols of the left and right of the flags
+                compile_flags = compile_flags.substr(1, compile_flags.length() - 2);
+                i++;
+            } else if (arg == "--output-ll-file") {
+                if (!n_args_follow(i + 1, "<file>", arg)) {
+                    return 1;
+                }
+                ll_file_path = get_absolute(cwd_path, args.at(i + 1));
+                i++;
+            } else {
+                print_err("Unknown argument: " + arg);
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    std::filesystem::path source_file_path = "";
+    std::filesystem::path out_file_path = "main";
+    std::string compile_flags;
+    std::filesystem::path ll_file_path = "";
+
+  private:
+    std::vector<std::string> args;
+    std::string argument_value;
+
+    static void print_help() {
+        std::cout << "Usage: flintc [OPTIONS]\n";
+        std::cout << std::endl;
+        std::cout << "Available Options:\n";
+        std::cout << "  --help, -h                  Show help\n";
+        std::cout << "  --file, -f <file>           The file to compile\n";
+        std::cout << "  --out, -o <file>            The name and path of the built output file\n";
+        std::cout << "  --flags \"[flags]\"           The clang flags used to build the executable\n";
+        std::cout << "  --output-ll-file <file>     Whether to output the compiled IR code.\n";
+        std::cout << "                              HINT: The compiler will still compile the input file as usual.\n";
+    }
+
+    static std::filesystem::path get_absolute(const std::filesystem::path &cwd, const std::string &path) {
+        std::filesystem::path file_path;
+        if (path[0] == '/') {
+            // Absolute path
+            file_path = path;
+        } else {
+            // Relative path
+            file_path = cwd / path;
+        }
+        return file_path;
+    }
+
+    static void print_err(const std::string &err) {
+        std::cerr << err << "\n";
+        print_help();
+    }
+
+    bool n_args_follow(const unsigned int count, const std::string &arg, const std::string &option) {
+        if (args.size() <= count) {
+            std::cerr << "Expected " << arg << " after '" << option << "' option!\n";
+            print_help();
+            return false;
+        }
+        return true;
+    }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "cli_parser.hpp"
 #include "generator/generator.hpp"
 #include "linker/linker.hpp"
 #include "parser/ast/file_node.hpp"
@@ -7,109 +8,59 @@
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 
-#include <filesystem> // to get cwd
+#include <filesystem>
 #include <iostream>
 #include <memory>
 #include <string>
-#include <sys/types.h>
-#include <system_error>
 
-void build_executable(const std::string &file_name) {
-    std::filesystem::path ll_file = std::filesystem::current_path() / "output.ll";
-    if (std::filesystem::exists(ll_file)) {
-        std::cout << "Using clang to build the executable '" << file_name << "' ..." << std::endl;
-        system(std::string("clang output.ll -o " + file_name).c_str());
-        // std::filesystem::remove(ll_file);
+/// build_executable
+///     Builds the executable file from the "output.ll" file
+void build_executable(const std::filesystem::path &ll_file_path, const std::filesystem::path &binary_path, const std::string &flags) {
+    if (std::filesystem::exists(ll_file_path)) {
+        std::cout << "Using clang to build the executable '" << binary_path.filename().string() << "' ..." << std::endl;
+        system(std::string("clang " + flags + " " + ll_file_path.string() + " -o " + binary_path.string()).c_str());
+        std::filesystem::remove(ll_file_path);
     }
 }
 
-class CommandLineParser {
-  public:
-    CommandLineParser(int argc, char *argv[]) {
-        // Convert the char* argv[] to a vector of strings
-        args.reserve(argc - 1);
-        for (size_t i = 1; i < argc; ++i) {
-            args.emplace_back(argv[i]);
-        }
+/// generate_ll_file
+///     Generates the "output.ll" file from a given source file
+void generate_ll_file(const std::filesystem::path &source_file_path, const std::filesystem::path &out_file_path,
+    const std::filesystem::path &ll_file_path) {
+    // Parse the .ft file and resolve all inclusions
+    FileNode file = Parser::parse_file(source_file_path);
+    Linker::resolve_links(file, source_file_path.parent_path());
+
+    // Generate the whole program
+    llvm::LLVMContext context;
+    std::unique_ptr<llvm::Module> program = Generator::generate_program_ir("main", context);
+
+    // Write the code to the normal output.ll file
+    std::error_code EC;
+    llvm::raw_fd_ostream ll_file(out_file_path.string(), EC);
+    if (!EC) {
+        ll_file << Generator::resolve_ir_comments(Generator::get_module_ir_string(program.get()));
+        ll_file.close();
     }
 
-    int parse() {
-        // Iterate through all command-line arguments
-        for (size_t i = 0; i < args.size(); ++i) {
-            std::string arg = args[i];
-
-            if (arg == "--help" || arg == "-h") {
-                print_help();
-            } else if (arg == "--file" || arg == "-f") {
-                if (!n_args_follow(i + 1, "<file>", arg)) {
-                    return 1;
-                }
-                std::filesystem::path cwd_path = std::filesystem::current_path();
-                std::filesystem::path file_path = cwd_path / args.at(i + 1);
-                std::string test_path = file_path.parent_path();
-
-                FileNode file = Parser::parse_file(file_path);
-                Linker::resolve_links(file, file_path.parent_path());
-
-                llvm::LLVMContext context;
-                std::unique_ptr<llvm::Module> program = Generator::generate_program_ir("main", context);
-                // std::cout << "IR_CODE:\n" << Generator::get_module_ir_string(program.get()) << std::endl;
-
-                // Write the code to a .ll file
-                std::error_code EC;
-                llvm::raw_fd_ostream ll_file("output.ll", EC);
-                if (!EC) {
-                    ll_file << Generator::resolve_ir_comments(Generator::get_module_ir_string(program.get()));
-                    ll_file.close();
-                }
-
-                // Clean up the module
-                Resolver::clear();
-                program->dropAllReferences();
-                program.reset();
-                ++i;
-            } else if (arg == "-o") {
-                if (!n_args_follow(i + 1, "<file>", arg)) {
-                    return 1;
-                }
-                build_executable(args[i + 1]);
-                return 0;
-            } else {
-                print_err("Unknown argument: " + arg);
-                return 1;
-            }
-        }
-        build_executable("main");
-        return 0;
+    // Copy the output.ll file to the ll_file_path, if the ll_file_path is declared
+    if (!ll_file_path.empty()) {
+        std::filesystem::copy_file(out_file_path, ll_file_path, std::filesystem::copy_options::overwrite_existing);
     }
 
-  private:
-    std::vector<std::string> args;
-    std::string argument_value;
-
-    static void print_help() {
-        std::cout << "Usage: [OPTIONS]\n";
-        std::cout << "--help, -h            Show help\n";
-        std::cout << "--file, -f <file>     The file to compile\n";
-        std::cout << "-o <file>             The name and path of the built output file\n";
-    }
-
-    static void print_err(const std::string &err) {
-        std::cerr << err << "\n";
-        print_help();
-    }
-
-    bool n_args_follow(const unsigned int count, const std::string &arg, const std::string &option) {
-        if (args.size() <= count) {
-            std::cerr << "Expected " << arg << " after '" << option << "' option!\n";
-            print_help();
-            return false;
-        }
-        return true;
-    }
-};
+    // Clean up the module
+    Resolver::clear();
+    program->dropAllReferences();
+    program.reset();
+}
 
 int main(int argc, char *argv[]) {
-    CommandLineParser command_line_parser(argc, argv);
-    return command_line_parser.parse();
+    CommandLineParser clp(argc, argv);
+    int result = clp.parse();
+    if (result != 0) {
+        return result;
+    }
+    generate_ll_file(clp.source_file_path, "output.ll", clp.ll_file_path);
+    build_executable("output.ll", clp.out_file_path, clp.compile_flags);
+    return 0;
 }
