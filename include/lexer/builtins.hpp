74190b83f6966e366a412c492fafa016ec068b6c
Marc Zweiler <marc.zweiler@outlook.at>
2025-02-19 01:02:03 +0100

    Added the type of builtin functions to the 'builtin_functions' map Renamed the 'BuiltinFunctions' enum to 'BuiltinFunction'

--- include/lexer/builtins.hpp
+++ include/lexer/builtins.hpp
@@ -4,7 +4,7 @@
 #include <string_view>
 #include <unordered_map>
 
-enum BuiltinFunctions { //
+enum BuiltinFunction { //
     PRINT,
     PRINT_ERR,
     ASSERT,
@@ -18,20 +18,20 @@ enum BuiltinFunctions { //
     SPLIT_ON_ALL,
 };
 
-static const std::unordered_map<std::string_view, BuiltinFunctions> builtin_functions = {
+static const std::unordered_map<std::string_view, std::pair<BuiltinFunction, std::string_view>> builtin_functions = {
     // printing
-    {"print", PRINT},        //
-    {"printerr", PRINT_ERR}, //
+    {"print", {PRINT, ""}},        //
+    {"printerr", {PRINT_ERR, ""}}, //
     // assertions
-    {"assert", ASSERT},         //
-    {"assert_arg", ASSERT_ARG}, //
+    {"assert", {ASSERT, ""}},         //
+    {"assert_arg", {ASSERT_ARG, ""}}, //
     // concurrency
-    {"run_on_all", RUN_ON_ALL},            //
-    {"map_on_all", MAP_ON_ALL},            //
-    {"filter_on_all", FILTER_ON_ALL},      //
-    {"reduce_on_all", REDUCE_ON_ALL},      //
-    {"reduce_on_pairs", REDUCE_ON_PAIRS},  //
-    {"partition_on_all", PARTITION_ON_ALL} //
+    {"run_on_all", {RUN_ON_ALL, ""}},            //
+    {"map_on_all", {MAP_ON_ALL, ""}},            //
+    {"filter_on_all", {FILTER_ON_ALL, ""}},      //
+    {"reduce_on_all", {REDUCE_ON_ALL, ""}},      //
+    {"reduce_on_pairs", {REDUCE_ON_PAIRS, ""}},  //
+    {"partition_on_all", {PARTITION_ON_ALL, ""}} //
 };
 
 #endif
--- src/generator/generator_expression.cpp
+++ src/generator/generator_expression.cpp
@@ -159,7 +159,7 @@ llvm::Value *Generator::Expression::generate_call(                         //
 
     // Check if it is a builtin function and call it
     if (builtin_functions.find(call_node->function_name) != builtin_functions.end()) {
-        llvm::Function *builtin_function = builtins.at(builtin_functions.at(call_node->function_name));
+        llvm::Function *builtin_function = builtins.at(builtin_functions.at(call_node->function_name).first);
         if (builtin_function == nullptr) {
             // Function has not been generated yet, but it should have been
             THROW_BASIC_ERR(ERR_NOT_IMPLEMENTED_YET);
