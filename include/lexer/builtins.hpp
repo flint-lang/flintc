1157d64d7fdca0e16ee0f7731421c4ba9f2aaf47
Marc Zweiler <marc.zweiler@outlook.at>
2025-02-26 23:26:29 +0100

    Added a 'builtin_function_types' map to the builtins.hpp file, which contains all parameter and return type overloads of the builtin functions (only 'print' for now) Added better documentation for all the content of the 'builtins.hpp' file

--- include/lexer/builtins.hpp
+++ include/lexer/builtins.hpp
@@ -3,7 +3,10 @@
 
 #include <string_view>
 #include <unordered_map>
+#include <vector>
 
+/// @enum `BuiltinFunction`
+/// @brief An enum describing the builtin function without saying anything about their respective parameter and return types
 enum BuiltinFunction { //
     PRINT,
     PRINT_ERR,
@@ -18,6 +21,8 @@ enum BuiltinFunction { //
     SPLIT_ON_ALL,
 };
 
+/// @var `builtin_functions`
+/// @brief A map mapping the name of a given builtin function (its string name) to the enum type of the builtin function
 static const std::unordered_map<std::string_view, BuiltinFunction> builtin_functions = {
     // printing
     {"print", PRINT},        //
@@ -33,6 +38,27 @@ static const std::unordered_map<std::string_view, BuiltinFunction> builtin_funct
     {"reduce_on_pairs", REDUCE_ON_PAIRS},  //
     {"partition_on_all", PARTITION_ON_ALL} //
 };
+
+/// @typedef `type_list`
+/// @brief This type is used for a list of types in the `builtin_function_types` map to make its signature much clearer
+using type_list = std::vector<std::string_view>;
+
+/// @var `builtin_function_types`
+/// @brief Map containing all argument and return types and all overloads of builtin functions
+static inline std::unordered_map<BuiltinFunction, std::vector<std::pair<type_list, type_list>>> builtin_function_types = {
+    {BuiltinFunction::PRINT,
+        {
+            {{"i32"}, {"void"}},
+            {{"i64"}, {"void"}},
+            {{"u32"}, {"void"}},
+            {{"u64"}, {"void"}},
+            {{"f32"}, {"void"}},
+            {{"f64"}, {"void"}},
+            {{"flint"}, {"void"}},
+            {{"char"}, {"void"}},
+            {{"str"}, {"void"}},
+            {{"bool"}, {"void"}},
+        }},
 };
 
 #endif
