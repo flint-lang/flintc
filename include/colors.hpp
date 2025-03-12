4359e574ce4c03a6e9a4d7f792c9a41c1061bc20
Marc Zweiler <marc.zweiler@outlook.at>
2025-03-12 21:29:00 +0100

    Changed colors to be inline

--- include/colors.hpp
+++ include/colors.hpp
@@ -2,9 +2,9 @@
 
 #include <string>
 
-static const std::string RED = "\033[31m";
-static const std::string GREEN = "\033[32m";
-static const std::string YELLOW = "\033[33m";
-static const std::string BLUE = "\033[34m";
-static const std::string WHITE = "\033[37m";
-static const std::string DEFAULT = "\033[0m";
+static const inline std::string RED = "\033[31m";
+static const inline std::string GREEN = "\033[32m";
+static const inline std::string YELLOW = "\033[33m";
+static const inline std::string BLUE = "\033[34m";
+static const inline std::string WHITE = "\033[37m";
+static const inline std::string DEFAULT = "\033[0m";
