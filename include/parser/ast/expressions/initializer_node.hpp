cb36f21409f1961f080eead10b9d851b2660bc09
Marc Zweiler <marc.zweiler@outlook.at>
2025-03-12 21:39:06 +0100

    Added new expression type: InitializerNode

---
+++ include/parser/ast/expressions/initializer_node.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "expression_node.hpp"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+/// @class `InitializerNode`
+/// @brief Represents all initializer expressions
+class InitializerNode : public ExpressionNode {
+  public:
+    InitializerNode(const std::string &type, const bool is_data, std::vector<std::unique_ptr<ExpressionNode>> &args) :
+        is_data(is_data),
+        args(std::move(args)) {
+        this->type = type;
+    }
+
+    /// @var `is_data`
+    /// @brief Determines whether the initialized type is a data or entity type
+    bool is_data;
+
+    /// @var `args`
+    /// @brief The arguments with which the initializer will be initialized
+    std::vector<std::unique_ptr<ExpressionNode>> args;
+};
