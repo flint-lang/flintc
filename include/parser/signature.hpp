a994f95545b2b466a7d00e44656faac338c4e0f2
Marc Zweiler <marc.zweiler@outlook.at>
2025-03-18 21:13:09 +0100

    Added multi-type signatures

--- include/parser/signature.hpp
+++ include/parser/signature.hpp
@@ -35,10 +35,12 @@ namespace Signature {
     const signature anytoken = {"#-?..#"};
     const signature type_prim = {"(", TOK_I32, "|", TOK_I64, "|", TOK_U32, "|", TOK_U64, "|", TOK_F32, "|", TOK_F64, "|", TOK_FLINT, "|",
         TOK_STR, "|", TOK_CHAR, "|", TOK_BOOL, ")"};
+    const signature type_prim_mult = {"(", TOK_I32X2, "|", TOK_I32X3, "|", TOK_I32X4, "|", TOK_I32X8, "|", TOK_I64X2, "|", TOK_I64X3, "|",
+        TOK_I64X4, "|", TOK_F32X2, "|", TOK_F32X3, "|", TOK_F32X4, "|", TOK_F32X8, "|", TOK_F64X2, "|", TOK_F64X3, "|", TOK_F64X4, ")"};
     const signature literal = {"(", TOK_STR_VALUE, "|", TOK_INT_VALUE, "|", TOK_FLINT_VALUE, "|", TOK_CHAR_VALUE, "|", TOK_TRUE, "|",
         TOK_FALSE, ")"};
     const signature type = combine({//
-        {"("}, type_prim, {"|(", TOK_IDENTIFIER, "))"}});
+        {"("}, type_prim, {"|", TOK_IDENTIFIER, "|"}, type_prim_mult, {")"}});
     const signature operational_binop = {"(", TOK_PLUS, "|", TOK_MINUS, "|", TOK_MULT, "|", TOK_DIV, "|", TOK_SQUARE, ")"};
     const signature relational_binop = {"(", TOK_EQUAL_EQUAL, "|", TOK_NOT_EQUAL, "|", TOK_LESS, "|", TOK_LESS_EQUAL, "|", TOK_GREATER, "|",
         TOK_GREATER_EQUAL, ")"};
