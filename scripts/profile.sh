8d92ebaa8ff3aab90e92bf0c6c2d5c26dee682e1
Marc Zweiler <marc.zweiler@outlook.at>
2025-07-12 21:19:25 +0200

    Updated the 'profile.sh' script, which can be used to profile *any* binary, not only the flint compiler

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -6,36 +6,69 @@ err_exit() {
     exit "$1"
 }
 
-# Get the projects root directory (use cd + dirname + pwd to always get absolute path, even when it is called relatively)
-root="$(cd "$(dirname "$0")" && cd .. && pwd)"
+if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    echo "profile [Option] <executable> [arguments with which to call the executable]*"
+    echo "Options:"
+    echo "  valgrind        Uses valgrind for performance analysis"
+    echo "  perf            Uses perf for performance analysis"
+fi
 
-# Make sure that 'perf' is installed
-[ ! -x "$(command -v perf)" ] && err_exit 1 "'perf' is not installed on your system! You can install it with your package manager (e.g. sudo apt install -y perf)"
+option="valgrind"
+if [ "$1" = "valgrind" ]; then
+    shift
+elif [ "$1" = "perf" ]; then
+    option="perf"
+    shift
+fi
 
-# Make sure that 'flamegraph.pl' is installed
-[ ! -x "$(command -v flamegraph.pl)" ] && err_exit 1 "'flamegraph.pl' is not installed on your system! You can install it with your package manager (e.g. sudo apt install -y flamegraph)"
+if [ "$option" = "perf" ]; then
+    # Make sure that 'perf' is installed
+    [ ! -x "$(command -v perf)" ] && err_exit 1 "'perf' is not installed on your system! You can install it with your package manager (e.g. sudo apt install -y perf)"
 
-# Make sure that 'stackcollapse-perf.pl' is installed
-[ ! -x "$(command -v stackcollapse-perf.pl)" ] && err_exit 1 "'stackcollapse-perf.pl' is not installed on your system! You can install it with your package manager (e.g. sudo apt install -y perf)"
+    # Make sure that 'flamegraph.pl' is installed
+    [ ! -x "$(command -v flamegraph.pl)" ] && err_exit 1 "'flamegraph.pl' is not installed on your system! You can install it with your package manager (e.g. sudo apt install -y flamegraph)"
 
-# Make sure that the executable (flintc) exists in the "$root/build/out/" directory
-[ ! -x "$root/build/out/flintc" ] && err_exit 1 "Executable 'flintc' not found in '$root/build/out/' directory!"
+    # Make sure that 'stackcollapse-perf.pl' is installed
+    [ ! -x "$(command -v stackcollapse-perf.pl)" ] && err_exit 1 "'stackcollapse-perf.pl' is not installed on your system! You can install it with your package manager (e.g. sudo apt install -y perf)"
+else
+    # Make sure that valgrind is installed
+    [ ! -x "$(command -v valgrind)" ] && err_exit 1 "'valgrind' is not installed!"
 
-# Make sure that cli arguments are given
-[ "$#" -eq 0 ] && err_exit 1 "No cli arguments given!"
+    # Make sure that kcachegrind is installed
+    [ ! -x "$(command -v kcachegrind)" ] && err_exit 1 "'kcachegrind' is not installed!"
+fi
+
+# Make sure that at least one cli argument (the file to profile) is given
+[ "$#" -gt 0 ] || err_exit 1 "No file given thats profilable! ('$1')"
+file="$1"
+shift
+
+# Make sure that the executable ($1) exists
+[ ! -x "$file" ] && err_exit 1 "Executable not found or not executable!"
 
 # Make sure the profiling directory exists but is empty
-mkdir -p "$root/scripts/profiling"
-rm -f "$root/scripts/profiling/"*
+mkdir -p "/tmp/profiling"
+rm -f "/tmp/profiling/"*
 
 echo "-- Starting Profiling"
 
-# Collect performance data with perf and forward all cli arguments of this script
-# shellcheck disable=SC2068 # We want to forward all arguments
-sudo perf record --call-graph dwarf --all-user -F 9999 -g -o "$root/scripts/profiling/perf.data" "$root/build/out/flintc" $@
+# Collect performance data with perf
+if [ "$option" = "perf" ]; then
+    # shellcheck disable=SC2068
+    perf record --call-graph fp --no-buildid-cache --no-buildid --all-user -F 10000 -g -o "/tmp/profiling/perf.data" "$file" $@
 
-# Generate the svg file
-sudo perf script -i "$root/scripts/profiling/perf.data" | stackcollapse-perf.pl | flamegraph.pl > "$root/scripts/profiling/out.svg"
+    # Generate the flamegraph
+    echo "-- Generating flamegraph..."
+    perf script -i "/tmp/profiling/perf.data" | stackcollapse-perf.pl > "/tmp/profiling/out.folded"
+    flamegraph.pl "/tmp/profiling/out.folded" > "/tmp/profiling/out.svg"
 
-# Visualize the flamegraph
-firefox "$root/scripts/profiling/out.svg"
+    # Open flamegraph
+    firefox "/tmp/profiling/out.svg"
+else
+    # Call valgrind
+    # shellcheck disable=SC2068
+    valgrind --tool=callgrind --callgrind-out-file=/tmp/profiling/callgrind.out "$file" $@
+
+    # Open the out file with kcachegrind
+    kcachegrind /tmp/profiling/callgrind.out &
+fi
