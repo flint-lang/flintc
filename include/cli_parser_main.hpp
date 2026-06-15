flint-lang/flintc

commit 0504f65286caa01e9611f0945060df57879ba21c

Renamed CLP field `build_exe` to `output_ll_file` since that's more clear what it does, removed `write_ll_file` function

include/cli_parser_main.hpp
@@ -296,7 +296,7 @@ class CLIParserMain : public CLIParserBase {
                     return 1;
                 }
                 ll_file_path = get_absolute(cwd_path, args.at(i + 1));
-                build_exe = false;
+                output_ll_file = false;
                 i++;
 #ifdef DEBUG_BUILD
             } else if (arg == "--profile-cumulative") {
@@ -368,7 +368,7 @@ class CLIParserMain : public CLIParserBase {
     std::filesystem::path out_file_path = "main";
     std::filesystem::path ll_file_path = "";
     std::vector<std::string> compile_flags;
-    bool build_exe{true};
+    bool output_ll_file{false};
     bool run{false};
     bool test{false};
     bool parallel{false};

src/main.cpp
@@ -21,21 +21,6 @@
 
 std::filesystem::path main_file_path;
 
-/// @function `write_ll_file`
-/// @brief Simply writes the given module to the given file, in IR source format
-///
-/// @param `ll_path` The path where to write the IR code to
-/// @param `module` The module containing the program to write to the given file
-void write_ll_file(const std::filesystem::path &ll_path, const llvm::Module *module) {
-    PROFILE_SCOPE("Write the ll file");
-    std::error_code EC;
-    llvm::raw_fd_ostream ll_file(ll_path.string(), EC);
-    if (!EC) {
-        ll_file << Generator::resolve_ir_comments(Generator::get_module_ir_string(module));
-        ll_file.close();
-    }
-}
-
 /// @function `compile_program`
 /// @brief Compiles the given program module down to a binary
 ///
@@ -167,12 +152,14 @@ int main(int argc, char *argv[]) {
     }
     Parser::clear_instances();
 
-    if (!clp.build_exe) {
-        // Output the built module and write it to the given file
-        write_ll_file(clp.ll_file_path, program.value().get());
-        // } else if (clp.run) {
-        // Run the IR code idrectly through JIT compilation
-        // TODO
+    if (clp.output_ll_file) {
+        PROFILE_SCOPE("Write the ll file");
+        std::error_code EC;
+        llvm::raw_fd_ostream ll_file(clp.ll_file_path.string(), EC);
+        if (!EC) {
+            ll_file << Generator::resolve_ir_comments(Generator::get_module_ir_string(program.value().get()));
+            ll_file.close();
+        }
     } else {
         // Compile the program and output the binary
         if (!NO_BINARY && !compile_program(clp.out_file_path, program.value().get(), clp.compile_flags, clp.is_static)) {

commit 689f1f8589f31e31d51ba80963c4092f41e97af0

Fixed incorrect ll file outputting flag bug introduced in 0504f65286caa01e9611f0945060df57879ba21c

include/cli_parser_main.hpp
@@ -296,7 +296,7 @@ class CLIParserMain : public CLIParserBase {
                     return 1;
                 }
                 ll_file_path = get_absolute(cwd_path, args.at(i + 1));
-                output_ll_file = false;
+                output_ll_file = true;
                 i++;
 #ifdef DEBUG_BUILD
             } else if (arg == "--profile-cumulative") {