4a681065bb7399cfd694c45606f84249b5242900
Marc Zweiler <marc.zweiler@outlook.at>
2026-09-24 23:29:46 +0200

    Fixed bug where storing `T`-typed values on a field of type `T?` in the initializer would crash the compiler as it would not emit a direct store but try to clone it instead

--- src/generator/generator_expression.cpp
+++ src/generator/generator_expression.cpp
@@ -3067,9 +3067,10 @@ Generator::group_mapping Generator::Expression::generate_initializer( //
                 }
                 const bool is_opaque = field.value->type->get_variation() == Type::Variation::OPAQUE;
                 const bool is_call = field.value->get_variation() == ExpressionNode::Variation::CALL;
+                const bool isnt_ptr_value = !expr_val->getType()->isPointerTy();
                 // Check if the element is freeable. If it is then we need to clone it before storing it in the field. We also assign it
                 // directly if it's an initalizer in itself, because then it has not been stored anywhere yet
-                if (!elem_type->is_freeable() || is_initializer || is_opt_literal || is_slice || is_opaque || is_call) {
+                if (!elem_type->is_freeable() || is_initializer || is_opt_literal || is_slice || is_opaque || is_call || isnt_ptr_value) {
                     IR::aligned_store(builder, expr_val, field_ptr);
                     continue;
                 }
